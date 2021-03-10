9fd359b85544a8c5e3bde674a417f145dad4e45f
Tobias Lengsholz <tobias.lengsholz@invision.de>
2021-03-10 22:09:31 +0100

    Add eslint plugin for graphql

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,5 @@
+const introspectionSchema = require("./introspection.json");
+
 const a11yLabelComponents = ["Label", "InputLabel"];
 const a11yControlComponents = [
   "Input",
@@ -25,6 +27,7 @@ module.exports = {
     "react-hooks",
     "import",
     "styled-components-a11y",
+    "graphql",
   ],
   env: {
     browser: true,
@@ -37,6 +40,25 @@ module.exports = {
   },
   reportUnusedDisableDirectives: true,
   rules: {
+    "graphql/template-strings": [
+      "error",
+      {
+        // Import default settings for your GraphQL client. Supported values:
+        // 'apollo', 'relay', 'lokka', 'fraql', 'literal'
+        env: "apollo",
+
+        // Import your schema JSON here
+        schemaJson: introspectionSchema,
+
+        // OR provide absolute path to your schema JSON (but not if using `eslint --cache`!)
+        // schemaJsonFilepath: path.resolve(__dirname, './schema.json'),
+
+        // OR provide the schema in the Schema Language format
+        // schemaString: printSchema(schema),
+
+        // tagName is gql by default
+      },
+    ],
     "no-restricted-syntax": [
       "error",
       { selector: "TSEnumDeclaration", message: "Don't declare enums" },
