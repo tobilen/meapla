c27b3cf534d4c810cd6bd676b069dae89ebb777c
Tobias Lengsholz <tobias.lengsholz@invision.de>
2021-03-13 15:33:13 +0100

    Use script for local introspection schema as well

--- scripts/get-graphql-schema.js
+++ scripts/get-graphql-schema.js
@@ -1,8 +1,18 @@
 #!/usr/bin/env node
 
 const { execSync } = require("child_process");
-require("dotenv").config({ path: "./.env.production.local" });
+require("dotenv").config({
+  path:
+    process.env.NODE_ENV === "production"
+      ? "./.env.production.local"
+      : "./.env.local",
+});
+
+const url =
+  process.env.NODE_ENV === "production"
+    ? "https://meapla-server.herokuapp.com"
+    : "http://localhost:4000";
 
 execSync(
-  `get-graphql-schema -h 'authorization="Bearer ${process.env.INTROSPECTION_QUERY_TOKEN}"' "https://meapla-server.herokuapp.com" > schema.graphql`
+  `get-graphql-schema -h 'authorization=Bearer ${process.env.INTROSPECTION_QUERY_TOKEN}' "${url}" > schema.graphql`
 );
