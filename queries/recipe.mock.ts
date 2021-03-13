fbd65c99d243790f7e76b7bf355648fb1f830f2a
Tobias Lengsholz <tobias.lengsholz@invision.de>
2021-03-13 20:49:33 +0100

    Add recipe queries and mutations

---
+++ queries/recipe.mock.ts
@@ -0,0 +1,126 @@
+import { MockedResponse } from "@apollo/client/testing";
+import {
+  Recipe,
+  Mutation,
+  MutationAddRecipeArgs,
+  MutationDeleteRecipeArgs,
+  MutationUpdateRecipeArgs,
+  Query,
+  QueryGetRecipeArgs,
+} from "../typings/graphql";
+import {
+  ADD_RECIPE_MUTATION,
+  DELETE_RECIPE_MUTATION,
+  GET_RECIPE_QUERY,
+  UPDATE_RECIPE_MUTATION,
+} from "./recipe";
+
+export const mockGetRecipe: (
+  recipe: Recipe
+) => MockedResponse<{
+  getRecipe: Query["getRecipe"];
+}> = (recipe) => {
+  const variables: QueryGetRecipeArgs = {
+    filter: { ids: [recipe.id] },
+  };
+
+  return {
+    request: {
+      query: GET_RECIPE_QUERY,
+      variables,
+    },
+    result: {
+      data: {
+        getRecipe: {
+          recipes: [recipe],
+        },
+      },
+    },
+  };
+};
+export const mockAddRecipe: (
+  recipe: Recipe
+) => MockedResponse<{
+  addRecipe: Mutation["addRecipe"];
+}> = (recipe) => {
+  const variables: MutationAddRecipeArgs = {
+    input: [
+      {
+        name: recipe.name,
+        ingredients: recipe.ingredients,
+      },
+    ],
+  };
+
+  return {
+    request: {
+      query: ADD_RECIPE_MUTATION,
+      variables,
+    },
+    result: {
+      data: {
+        addRecipe: {
+          recipes: [recipe],
+        },
+      },
+    },
+  };
+};
+
+export const mockDeleteRecipe: (
+  recipe: Recipe
+) => MockedResponse<{
+  deleteRecipe: Mutation["deleteRecipe"];
+}> = (recipe) => {
+  const variables: MutationDeleteRecipeArgs = {
+    filter: {
+      ids: [recipe.id],
+    },
+  };
+
+  return {
+    request: {
+      query: DELETE_RECIPE_MUTATION,
+      variables,
+    },
+    result: {
+      data: {
+        deleteRecipe: {
+          recipes: [recipe],
+        },
+      },
+    },
+  };
+};
+
+export const mockUpdateRecipe: (
+  recipe: Recipe
+) => MockedResponse<{
+  updateRecipe: Mutation["updateRecipe"];
+}> = (recipe) => {
+  const variables: MutationUpdateRecipeArgs = {
+    input: {
+      filter: {
+        ids: [recipe.id],
+      },
+      set: {
+        name: recipe.name,
+        ingredients: recipe.ingredients,
+      },
+    },
+  };
+
+  return {
+    request: {
+      query: UPDATE_RECIPE_MUTATION,
+      variables,
+    },
+    result: {
+      data: {
+        updateRecipe: {
+          recipes: [recipe],
+        },
+      },
+    },
+  };
+};
---
+++ queries/recipe.ts
@@ -0,0 +1,69 @@
+import { gql } from "@apollo/client";
+
+export const GET_RECIPE_QUERY = gql`
+  query getRecipe($input: RecipeFilter!) {
+    getRecipe(filter: $input) {
+      recipes {
+        id
+        name
+        ingredients {
+          id
+          name
+          amount
+          measurement
+        }
+      }
+    }
+  }
+`;
+
+export const ADD_RECIPE_MUTATION = gql`
+  mutation addRecipe($input: [RecipeInput!]!) {
+    addRecipe(input: $input) {
+      recipes {
+        id
+        name
+        ingredients {
+          id
+          name
+          amount
+          measurement
+        }
+      }
+    }
+  }
+`;
+
+export const UPDATE_RECIPE_MUTATION = gql`
+  mutation updateRecipe($input: RecipeUpdateInput!) {
+    updateRecipe(input: $input) {
+      recipes {
+        id
+        name
+        ingredients {
+          id
+          name
+          amount
+          measurement
+        }
+      }
+    }
+  }
+`;
+
+export const DELETE_RECIPE_MUTATION = gql`
+  mutation deleteRecipe($filter: RecipeFilter!) {
+    deleteRecipe(filter: $filter) {
+      recipes {
+        id
+        name
+        ingredients {
+          id
+          name
+          amount
+          measurement
+        }
+      }
+    }
+  }
+`;
