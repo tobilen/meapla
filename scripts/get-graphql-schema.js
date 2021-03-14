c56fde6b5147c6a25ef4226b872dbbc0f5cdc476
Tobias Lengsholz <tobias.lengsholz@invision.de>
2021-03-14 16:22:48 +0100

    Generate short lived token for each introspection query

--- scripts/get-graphql-schema.js
+++ scripts/get-graphql-schema.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 const { execSync } = require("child_process");
+const jwt = require("jsonwebtoken");
+
 require("dotenv").config({
   path:
     process.env.NODE_ENV === "production"
@@ -13,6 +15,20 @@ const url =
     ? "https://meapla-server.herokuapp.com"
     : "http://localhost:4000";
 
+const authorization = `Bearer ${jwt.sign(
+  {
+    iat: Date.now() / 1000,
+    exp: Math.floor(Date.now() / 1000) + 300,
+    "https://meapla.vercel.app/jwt/claims": {
+      "x-ci-token": true,
+    },
+  },
+  process.env.NEXTAUTH_JWT_SECRET || "",
+  {
+    algorithm: "HS256",
+  }
+)}`;
+
 execSync(
-  `get-graphql-schema -h 'authorization=Bearer ${process.env.INTROSPECTION_QUERY_TOKEN}' "${url}" > schema.graphql`
+  `get-graphql-schema -h 'authorization=${authorization}' "${url}" > schema.graphql`
 );
